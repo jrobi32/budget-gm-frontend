99f1ff6bb2a4206bacc3729d2fa316b685fd6acd
jrobi32 <johnnyau23@gmail.com>
2025-04-16 00:43:10 -0500

    Use NBA.com player IDs for reliable image loading

---
+++ src/playerIds.js
@@ -0,0 +1,25 @@
+// NBA.com player IDs for image URLs
+export const playerIds = {
+    'Kevin Durant': '201142',
+    'LeBron James': '2544',
+    'Stephen Curry': '201939',
+    'Giannis Antetokounmpo': '203507',
+    'Luka Doncic': '1629029',
+    'Joel Embiid': '203954',
+    'Jayson Tatum': '1628369',
+    'Devin Booker': '1626164',
+    'Anthony Edwards': '1630162',
+    'Karl-Anthony Towns': '1626157',
+    'Cade Cunningham': '1630595',
+    'Naz Reid': '1629675',
+    'Josh Giddey': '1630581',
+    'Scottie Barnes': '1630567',
+    'Jaren Jackson Jr.': '1628991',
+    'Donte DiVincenzo': '1628978',
+    'Andrew Nembhard': '1630612',
+    'Duncan Robinson': '1629130',
+    'Royce O\'Neale': '1626220',
+    'De\'Aaron Fox': '1628368',
+    'Bennedict Mathurin': '1631097',
+    'Kyle Filipowski': '1641712'
+}; 
