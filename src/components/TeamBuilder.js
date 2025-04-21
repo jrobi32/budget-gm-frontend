ea6f7967d54c0eed0d61b5157c993478489c6c14
jrobi32 <johnnyau23@gmail.com>
2025-04-21 17:03:40 -0500

    fix: comprehensive update to fix ESLint and build issues

--- src/components/TeamBuilder.js
+++ src/components/TeamBuilder.js
@@ -105,7 +105,7 @@ const TeamBuilder = ({ onError, nickname }) => {
             }
         };
         initializeApp();
-    }, [loadPlayerPool]);
+    }, []);
 
     const updatePlayerOptions = (pool, currentSelectedPlayers) => {
         const selectedPlayerNames = currentSelectedPlayers.map(p => p.name);
